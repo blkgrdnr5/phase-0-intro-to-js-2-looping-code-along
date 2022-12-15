4323f85fa47f5168f9ab3f2d0e28025895405d28
Blake Gardner <blkgrdnr5@gmail.com>
2022-12-14 19:37:36 -0600

    commit tree b083d0568a547f0c53264faabc8ca0766d0418dc

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"];
+const event = ["Birthday"]
+
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(down){
+    while (down >= 0) {
+        console.log(down--)
+    }
+    return down
+}
+
